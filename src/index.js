98c517b82e94c645e694ad99211f42c7bd3ed963
s.mash <s.mashtakov@gmail.com>
2019-08-29 13:06:12 +0300

    fro test lint

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
+console.log(`Hello, ${actual}!`);
 };
