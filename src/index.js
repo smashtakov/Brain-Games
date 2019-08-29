5479e2bf4ecc3737eab43f39930e4393a1ffc018
s.mash <s.mashtakov@gmail.com>
2019-08-29 11:40:50 +0300

    4th step

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@
 
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-
-console.log('Welcome to the Brain Games!');
-const actual = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + actual + '!');
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
 };
